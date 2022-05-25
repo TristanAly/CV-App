ce2bd99f03672e8b4fbdb4c012b905de64e79211
AlyTristan <tristan.aly@gmail.com>
2022-05-25 21:54:12 +0200

    Initial Commit

---
+++ CV App/CV_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CV_AppApp.swift
+//  CV App
+//
+//  Created by Tristan Aly on 25/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CV_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
